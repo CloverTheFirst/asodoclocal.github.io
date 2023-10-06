28c2ee97ff6b87de94676f871e7efc2bd6d40d9e
CloverTheFirst <122287144+CloverTheFirst@users.noreply.github.com>
2023-10-06 13:01:35 +0200

    Create test_java.cpp

---
+++ test_java.cpp
@@ -0,0 +1,15 @@
+#include iostream;
+#using namespace std;
+
+int main void {
+
+    int list [a, b, c, d];
+
+    int i; 
+
+    for (i=0, i<4, i++)
+    {
+        cout>>i>>"">>"1">>endl;
+    }
+
+} 
