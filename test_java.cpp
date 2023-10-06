d2740f6ecc38905159a384307d8317811b49950b
CloverTheFirst <122287144+CloverTheFirst@users.noreply.github.com>
2023-10-06 15:06:35 +0200

    Update test_java.cpp

--- test_java.cpp
+++ test_java.cpp
@@ -16,3 +16,7 @@ int main() {
 
     return 0;
 }
+
+hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.2.jar
+-files mapper.py,reducer.py -mapper mapper.py -reducer reducer.py
+-input /data/miserables -output wc
